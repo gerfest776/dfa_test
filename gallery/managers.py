c46e35223bd830d41d3a222b7f9a8981c22717bf
Герман Гриценко <gerfest.gricenko@gmail.com>
2022-09-08 22:33:09 +0300

    feat: dockerize

--- gallery/managers.py
+++ gallery/managers.py
@@ -1,7 +1,7 @@
 from django.db.models import Manager
 
 
-class CustomManager(Manager):
+class GalleryManager(Manager):
     def delete(self):
         for obj in self.get_queryset():
             obj.delete()
